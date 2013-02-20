6c3a356dbbc1726646c908561c8c935439de38bf
Tor Håvard Bieltvedt <konbietor@gmail.com>
2013-02-20 15:07:10 +0100

    Added new set of shell-config files with .sh extension.

---
+++ shell-config/bash_profile_mac_variables.include.sh
@@ -0,0 +1,14 @@
+### Base directories ###
+DEV_SOFTWARE_HOME=/Users/THB-Tine/Dev-Software-Tine
+DEV_HOME=~/Dev
+GIT_REPOSITORIES_HOME=$DEV_HOME/Git-Repositories
+
+### Home directories export ###
+#export JAVA_HOME=$DEV_SOFTWARE_HOME/Java/jdk1.6.0_38
+#export GIT_HOME=$DEV_SOFTWARE_HOME/Git/Git-1.7.11
+export MVN_HOME=$DEV_SOFTWARE_HOME/Maven/apache-maven-3.0.4
+export JREBEL_HOME=$DEV_SOFTWARE_HOME/JRebel/jrebel-5.1.2
+
+### Windows special modification ###
+# (Remove this if Linux/Mac)
+#export PATH=/usr/local/bin:/usr/bin:/cygdrive/c/Windows/system32:/cygdrive/c/Windows
